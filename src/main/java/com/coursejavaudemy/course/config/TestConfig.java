34b6bd3d5de1ba2d87562f9b8bfdaff265592727
Robert Koch <robertkoch117@gmail.com>
2021-09-23 08:53:50 -0300

    Payment, one-to-one association

--- src/main/java/com/coursejavaudemy/course/config/TestConfig.java
+++ src/main/java/com/coursejavaudemy/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.coursejavaudemy.course.entities.Category;
 import com.coursejavaudemy.course.entities.Order;
 import com.coursejavaudemy.course.entities.OrderItem;
+import com.coursejavaudemy.course.entities.Payment;
 import com.coursejavaudemy.course.entities.Product;
 import com.coursejavaudemy.course.entities.User;
 import com.coursejavaudemy.course.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
